b132e164f427e3ac77f1e7fcf9e667a2fb4c3b14
Peter Jang <peter.x.jang@gmail.com>
2016-06-23 12:25:09 -0500

    Add Person model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  Person.create(name: Faker::Name.name, bio: Faker::Lorem.paragraph)
+end

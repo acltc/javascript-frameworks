296a26ef75b84b19e63008054acd811d252aa9b8
Peter Jang <peter.x.jang@gmail.com>
2016-06-23 15:16:36 -0500

    Toggle the bio on click

--- app/assets/javascripts/components/person.js.jsx
+++ app/assets/javascripts/components/person.js.jsx
@@ -1,14 +1,24 @@
 /* global React */
 
 var Person = React.createClass({
+  getInitialState: function() {
+    return {
+      bioVisible: true
+    };
+  },
+  toggleBio: function() {
+    this.setState({
+      bioVisible: !this.state.bioVisible
+    });
+  },
   handleClick: function() {
     this.props.onDeletePerson(this.props.id);
   },
   render: function() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
-        <p>{this.props.bio}</p>
+        <h2 onClick={this.toggleBio}>{this.props.name}</h2>
+        { this.state.bioVisible ? <p>{this.props.bio}</p> : null }
         <a href="#" onClick={this.handleClick}>Delete</a>
       </div>
     );

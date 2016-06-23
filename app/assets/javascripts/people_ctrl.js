5310abace70bfd90882aeb3dc61e66960da1ac4f
Peter Jang <peter.x.jang@gmail.com>
2016-06-23 14:02:28 -0500

    Display all the people (using fixture data)

---
+++ app/assets/javascripts/people_ctrl.js
@@ -0,0 +1,36 @@
+/* global angular */
+
+(function() {
+  angular.module('app').controller('peopleCtrl', function($scope) {
+    $scope.people = [
+      {
+        id: 1,
+        name: "Jackie Kuhlman",
+        bio: "Qui quibusdam aut. Iusto est numquam est excepturi aspernatur quia omnis. Perferendis aliquam qui nisi nemo. Et sunt hic molestiae voluptate."
+      },
+      {
+        id: 2,
+        name: "Ivah Kautzer",
+        bio: "Autem numquam qui quas. Veniam animi ut. Ut porro voluptatem laboriosam fugit temporibus sint soluta. Et aut autem iure. Beatae ea quo labore quaerat et."
+      },
+      {
+        id: 3,
+        name: "Alice Goodwin",
+        bio: "Dolores laboriosam et rerum. Nihil explicabo quos. Commodi officiis architecto ad quibusdam aliquid consequuntur. Accusantium dolore quidem corporis est non debitis."
+      },
+      {
+        id: 4,
+        name: "Danyka Renner",
+        bio: "Vitae unde aliquid. Reprehenderit in itaque quae est et et temporibus. Laboriosam et aliquam tempore beatae. Rerum iure mollitia enim."
+      },
+      {
+        id: 5,
+        name: "Hipolito Orn",
+        bio: "Aperiam voluptate sed ipsam nihil ut et. Et perspiciatis consequatur tempora deserunt nesciunt eaque fugiat. Enim recusandae eum et. Dolore dolorum nobis et et."
+      }
+    ];
+
+    window.scope = $scope;
+  });
+})();
+

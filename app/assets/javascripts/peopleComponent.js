495aa788972c0757824e55a8f1cce32d3a832c44
Peter Jang <peter.x.jang@gmail.com>
2016-06-24 11:29:06 -0500

    Display all the people (using fixture data)

--- app/assets/javascripts/peopleComponent.js
+++ app/assets/javascripts/peopleComponent.js
@@ -1,10 +1,42 @@
 (function(app) {
   app.AppComponent = ng.core.Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+      <div *ngFor="let person of people">
+        <h2>{{ person.name}}</h2>
+        <p>{{ person.bio }}</p>
+      </div>`
   })
   .Class({
-    constructor: function() {}
+    constructor: function() {
+      this.people = [
+        {
+          id: 1,
+          name: "Jackie Kuhlman",
+          bio: "Qui quibusdam aut. Iusto est numquam est excepturi aspernatur quia omnis. Perferendis aliquam qui nisi nemo. Et sunt hic molestiae voluptate."
+        },
+        {
+          id: 2,
+          name: "Ivah Kautzer",
+          bio: "Autem numquam qui quas. Veniam animi ut. Ut porro voluptatem laboriosam fugit temporibus sint soluta. Et aut autem iure. Beatae ea quo labore quaerat et."
+        },
+        {
+          id: 3,
+          name: "Alice Goodwin",
+          bio: "Dolores laboriosam et rerum. Nihil explicabo quos. Commodi officiis architecto ad quibusdam aliquid consequuntur. Accusantium dolore quidem corporis est non debitis."
+        },
+        {
+          id: 4,
+          name: "Danyka Renner",
+          bio: "Vitae unde aliquid. Reprehenderit in itaque quae est et et temporibus. Laboriosam et aliquam tempore beatae. Rerum iure mollitia enim."
+        },
+        {
+          id: 5,
+          name: "Hipolito Orn",
+          bio: "Aperiam voluptate sed ipsam nihil ut et. Et perspiciatis consequatur tempora deserunt nesciunt eaque fugiat. Enim recusandae eum et. Dolore dolorum nobis et et."
+        }
+      ];
+    }
   });
 
   document.addEventListener('DOMContentLoaded', function() {

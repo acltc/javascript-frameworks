78266ddb667c1df44e485cc82f8743eb24777d73
Peter Jang <peter.x.jang@gmail.com>
2016-06-23 20:27:26 -0500

    Change the bio styling on click

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -8,31 +8,31 @@ new Vue({
         id: 1,
         name: "Jackie Kuhlman",
         bio: "Qui quibusdam aut. Iusto est numquam est excepturi aspernatur quia omnis. Perferendis aliquam qui nisi nemo. Et sunt hic molestiae voluptate.",
-        bioVisible: true
+        bioStrikeThrough: false
       },
       {
         id: 2,
         name: "Ivah Kautzer",
         bio: "Autem numquam qui quas. Veniam animi ut. Ut porro voluptatem laboriosam fugit temporibus sint soluta. Et aut autem iure. Beatae ea quo labore quaerat et.",
-        bioVisible: true
+        bioStrikeThrough: false
       },
       {
         id: 3,
         name: "Alice Goodwin",
         bio: "Dolores laboriosam et rerum. Nihil explicabo quos. Commodi officiis architecto ad quibusdam aliquid consequuntur. Accusantium dolore quidem corporis est non debitis.",
-        bioVisible: true
+        bioStrikeThrough: false
       },
       {
         id: 4,
         name: "Danyka Renner",
         bio: "Vitae unde aliquid. Reprehenderit in itaque quae est et et temporibus. Laboriosam et aliquam tempore beatae. Rerum iure mollitia enim.",
-        bioVisible: true
+        bioStrikeThrough: false
       },
       {
         id: 5,
         name: "Hipolito Orn",
         bio: "Aperiam voluptate sed ipsam nihil ut et. Et perspiciatis consequatur tempora deserunt nesciunt eaque fugiat. Enim recusandae eum et. Dolore dolorum nobis et et.",
-        bioVisible: true
+        bioStrikeThrough: false
       }
     ],
     newPersonName: '',
@@ -52,7 +52,7 @@ new Vue({
       this.people.splice(index, 1);
     },
     toggleBio: function(person) {
-      person.bioVisible = !person.bioVisible;
+      person.bioStrikeThrough = !person.bioStrikeThrough;
     }
   }
 });
